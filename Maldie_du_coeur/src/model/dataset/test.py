df89ae079e130f96475f06804e7bfb52a4d87496
irtegralk-ux <irtegramk@gmail.com>
2025-12-13 12:46:19 +0100

    Rapport d'analyse et analyse de donnéés

--- Maldie_du_coeur/src/model/dataset/test.py
+++ Maldie_du_coeur/src/model/dataset/test.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 dossier = pd.read_csv("Maldie_du_coeur/datasets/raw_data/heart_disease.csv", header=None)
 print(dossier.head())
+print(dossier.describe())
 print(dossier.shape)
